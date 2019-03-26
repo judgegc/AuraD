8809e4382652bbba6f3574ae064ddcdeecc470fa
JudgeGC <judgegc@gmail.com>
2019-03-26 11:45:22 +0200

    Remove unused code

--- AuraD/Src/Log/Logger.cpp
+++ AuraD/Src/Log/Logger.cpp
@@ -10,9 +10,6 @@ void Logger::Log(const std::wstring & formatted)
 {
 	using namespace std::string_literals;
 
-	auto now = std::chrono::system_clock::now();
-	auto in_time_t = std::chrono::system_clock::to_time_t(now);
-
 	Write(L"["s + Core::Timestamp() + L"] "s + formatted + newline);
 }
 }
